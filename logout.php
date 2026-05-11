1ca24369c4d509be4cd1bd2007c711314d26c145
SDemonteverde <38735789+Shyed@users.noreply.github.com>
2026-05-11 15:48:09 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,10 @@
 <?
-session_start();
-session_unset();
-session_destroy();
-ob_start();
-header("location:index.php");
-ob_end_flush(); 
+session_start(); // Start the current session
+session_unset(); // Remove all session variables
+session_destroy(); // Destroy the active session completely
+ob_start(); // Start output buffering to prevent header errors
+header("location:index.php"); // Redirect user back to homepage after logout
+ob_end_flush();  // Flush output buffer and send response
 include 'index.php';
 //include 'home.php';
 exit();
